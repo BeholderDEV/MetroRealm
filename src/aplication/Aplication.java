4b325465c947fefe3552e6ade8f4eeb9eb5d4f5f
AlissonSteffens <alisson.steffens@hotmail.com>
2016-12-01 20:49:49 -0200

    adicionando regras

--- src/aplication/Aplication.java
+++ src/aplication/Aplication.java
@@ -5,6 +5,10 @@
  */
 package aplication;
 
+import java.io.File;
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+
 /**
  *
  * @author Alisson
@@ -16,6 +20,28 @@ public class Aplication {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        File file = new File("src/aplication/tipper.flc");
+        FIS fis = FIS.load(file.getPath(),true);
+        
+        if (fis == null){
+            System.err.println("Eitaaa");
+        }
+        
+        JFuzzyChart.get().chart(fis);
+//
+//        // Set inputs
+//        fis.setVariable("service", 3);
+//        fis.setVariable("food", 7);
+//
+//        // Evaluate
+//        fis.evaluate();
+//
+//        // Show output variable's chart
+//        Variable tip = functionBlock.getVariable("tip");
+//        JFuzzyChart.get().chart(tip, tip.getDefuzzifier(), true);
+
+        // Print ruleSet
+        System.out.println(fis);
     }
     
 }
